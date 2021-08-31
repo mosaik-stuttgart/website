0fabb47cbfa8cf45aefcf461bd7349ef7029edd2
Silas <krausesilas@gmail.com>
2021-08-31 14:22:12 +0200

    Update Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,48 @@ module.exports = {
 	 './resources/**/*.php',
 	  './content/**/*.md'
   ],
+  mode: 'jit',
   theme: {
-    typography: {
-      default: {
-        css: {
-          lineHeight: '1.5',
-          color: false,
-          fontWeight: false,
-          a: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            lineHeight: '1.5',
             color: false,
-            '&:hover': {
+            fontWeight: false,
+            a: {
               color: false,
+              '&:hover': {
+                color: false,
+              },
             },
           },
         },
       },
-    },
-    extend: {
       colors: {
-        black: '#010001'
+        black: '#010001',
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
+        blue: {
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#90cdf4',
+          400: '#63b3ed',
+          500: '#4299e1',
+          600: '#3182ce',
+          700: '#2b6cb0',
+          800: '#2c5282',
+          900: '#2a4365',
+        },
       },
       width: {
         '28': '7rem'
@@ -44,7 +67,10 @@ module.exports = {
       maxHeight: {
         'xl': '36rem'
       },
-      scale: { '-100': '-1'}
+      scale: { '-100': '-1'},
+      boxShadow: {
+        xs: '0 0 0 1px rgba(0, 0, 0, 0.05)'
+      }
     },
     fontFamily: {
       sans: defaultTheme.fontFamily.sans,
